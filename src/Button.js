a06b7a8a85862fab85f7792eea1498042da33c9e
Osama Alvi <osamaalvie@gmail.com>
2020-02-06 10:09:05 +0400

    task4

---
+++ src/Button.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {withGlobalState} from 'react-globally'
+import axios from 'axios';
+import Table from "./Table";
+
+class Button extends Component {
+
+    constructor(props) {
+        console.log("Button Constructor");
+        //console.log(props);
+        super(props);
+        this.state = {
+            urls: [
+                "http://localhost/starwbackend/public/api/movies/byLongestCrawl",
+                "http://localhost/starwbackend/public/api/movies/characterByMostAppearance",
+                "http://localhost/starwbackend/public/api/movies/speciesByMostAppearance",
+                "http://localhost/starwbackend/public/api/movies/largestNoOfVehicle",
+            ]
+        };
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
+    render() {
+        console.log("Button render");
+        return <button className="btn btn-warning btn-block" onClick={this.fetch}>Search</button>;
+    }
+
+    fetch = () => {
+        const url = this.state.urls[this.props.globalState.counter];
+
+        axios.get(url, {
+            headers: {},
+
+        })
+            .then((response) => {
+                // console.log(response);
+
+                ReactDOM.render(<Table data={response} counter={this.props.globalState.counter}/>, document.getElementById('table'));
+
+
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log(error.response.headers);
+                }
+                else if (error.request) {
+                    console.log(error.request);
+                }
+                else {
+                    console.log(error.message);
+                }
+
+
+            });
+    }
+
+}
+
+export default withGlobalState(Button)
